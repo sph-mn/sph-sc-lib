83665135d0bd7273cbbce99904fd7a36975848b7
sph <sph@posteo.eu>
2016-11-05 14:18:53 +0000

    fix mi-list tests. successful memory-leak check for mi-list tests

--- c-versions/test/sph/mi-list.c
+++ c-versions/test/sph/mi-list.c
@@ -1,9 +1,20 @@
 
+#ifndef sc_included_stdio_h
 #include <stdio.h>
+#define sc_included_stdio_h
+#endif
+#ifndef sc_included_inttypes_h
 #include <inttypes.h>
+#define sc_included_inttypes_h
+#endif
+#ifndef sc_included_assert_h
 #include <assert.h>
+#define sc_included_assert_h
+#endif
+#ifndef sc_included_time_h
 #include <time.h>
-
+#define sc_included_time_h
+#endif
 #ifndef sc_included_stdlib_h
 #include <stdlib.h>
 #define sc_included_stdlib_h
@@ -79,15 +90,15 @@ size_t mi_list_name(length)(mi_list_t* a) {
 
 ;
 #define test_element_count 100
-mi_list_t* insert_values(mi_list_t* a) {
+mi_list_64_t* insert_values(mi_list_64_t* a) {
     size_t counter=test_element_count;
     while(counter) {
-        a=mi_list_add(a,counter);
+        a=mi_list_64_add(a,counter);
         counter=(counter-1);
     };
-    mi_list_add(a,counter);
+    mi_list_64_add(a,counter);
 };
-uint8_t test_value_existence(mi_list_t* a) {
+uint8_t test_value_existence(mi_list_64_t* a) {
     size_t counter=0;
     while((counter<=test_element_count)) {
         assert((counter==mi_list_first(a)));
@@ -95,7 +106,7 @@ uint8_t test_value_existence(mi_list_t* a) {
         counter=(counter-1);
     };
 };
-void print_contents(mi_list_t* a) {
+void print_contents(mi_list_64_t* a) {
     printf("print-contents\n");
     while(a) {
         printf("%lu\n",mi_list_first(a));
@@ -105,11 +116,11 @@ void print_contents(mi_list_t* a) {
 #define get_time() ((uint64_t)(time(0)))
 #define print_time(a) printf("%u\n",a)
 int main() {
-    mi_list_t* a=mi_list_create();
+    mi_list_64_t* a=0;
     a=insert_values(a);
     test_value_existence(a);
     print_contents(a);
-    mi_list_destroy(a);
+    mi_list_64_destroy(a);
     printf("success\n");
     return(0);
 };
