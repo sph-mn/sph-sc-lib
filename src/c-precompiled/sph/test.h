5a5a460f2790df1f8094159345b090a22f648508
sph <sph@posteo.eu>
2025-08-02 21:25:17 +0200

    add test.h

---
+++ src/c-precompiled/sph/test.h
@@ -0,0 +1,17 @@
+
+/* depends on status.h */
+
+#define test_helper_test_one(func) \
+  printf("%s\n", #func); \
+  status_require((func()))
+#define test_helper_assert(description, expression) \
+  if (!expression) { \
+    printf("%s failed\n", description); \
+    status_set_goto("", 1); \
+  }
+#define test_helper_display_summary() \
+  if (status_is_success) { \
+    printf(("--\ntests finished successfully.\n")); \
+  } else { \
+    printf(("\ntests failed. %d %s\n"), (status.id), (sp_status_description(status))); \
+  }
